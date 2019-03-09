fb3efc4d9d3cd906ca18aaf51ead8605fa0ea8ec
Senecacherry <42197371+Senecacherry@users.noreply.github.com>
2019-03-09 10:22:37 -0700

    app.js work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Dogs from "./components/dogs";
+import Scores from "./components/Scores";
+import Header from "./components/Header"
+import dogs from "./dogs.json";
+import Data from "./components/History"
+import Wrapper from "./components/Wrapper"
 import './App.css';
 
 class App extends Component {
+
+  state= {
+
+    dogs,
+    score: 0,
+    value: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    target: 0,
+    wins: 0,
+    loses:0
+
+  };
+
+  componentDidMount = () => {
+
+    this.updateValues()
+    this.setTarget()
+
+  }
+
+  handleScore = (id, value) => {
+
+    this.setState({score: this.state.score + value})
+    if (this.state.score > this.state.target) {
+      let newLoses = this.state.loses + 1
+      this.setState({score: 0, loses:})
+      this.updateValues()
+      this.setTarget()
+    } else if (this.state.score === this.state.target) {
+      let newWins = this.state.wins + 1
+      this.setState({score: 0, wins: newWins})
+      this.updateValues()
+      this.setTarget()
+    }
+  };
+
+  updateValues=()=>{
+    let newValues = this.state.value.map(x => (x = (Math.floor(Math.random() * 12) + 1)))
+    this.setState({value: newValues})
+  }
+
+  setTarget=()=>{
+    let newTarget = (Math.floor(Math.random() * (120-19+1)));
+    this.setState({target: newTarget})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header />
+        <Scores target={this.state.target}
       </div>
     );
   }
