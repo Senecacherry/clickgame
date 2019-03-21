b61df672f19b6f2aaf20047b688ee68c74a37fd3
Senecacherry <42197371+Senecacherry@users.noreply.github.com>
2019-03-21 17:44:08 -0600

    Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+    return (
+        <header className="header">
+        <h1>Click Game!!</h1>
+        <h2>Clcik on an image to earn points, dont go over the target score or you lose!</h2>
+        </header>
+    );
+}
+
+export default Header;
